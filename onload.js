8c32c8bb79b80f8192bf706f35c3de4f6ae2ae78
Noteworthy A Cappella LHS <38771589+NoteworthyAcappella@users.noreply.github.com>
2019-05-28 19:27:31 -0400

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,1 +1,34 @@
-
+function onload(){
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  width = window.innerWidth;
+  height = window.innerHeight;
+  width = width/2;
+  width = width - (width/20);
+  height = width * (9/16);
+  width = width.toString();
+  fullHeight = height.toString();
+  width += "px";
+  fullHeight += "px";
+  document.getElementsByClassName("right")[0].style.right = 0;
+  var x = document.getElementsByClassName("widthed");
+  var i;
+  for (i = 0; i < x.length; i++){
+    x[i].style.width = width;
+    x[i].style.height = fullHeight;
+  }
+  if (height < 200){
+    height = 200;
+  }
+  height = height.toString();
+  height += "px";
+  var y = document.getElementsByClassName("hello");
+  for (i = 0; i < y.length; i++){
+    y[i].style.width = width;
+    y[i].style.height = height;
+  }
+  var z = document.getElementsByClassName("vid");
+  for (i = 0; i < z.length; i++){
+    z[i].style.height = height;
+  }
+}
